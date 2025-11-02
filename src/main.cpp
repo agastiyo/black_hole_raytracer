cad47074e73d740aab8bb49a676661a802a9d56d
Agastya Gaur <73049397+agastiyo@users.noreply.github.com>
2025-11-01 21:18:25 -0500

    Add sphere to scene

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,24 @@
 
 using namespace std;
 
+bool hit_sphere(const vec3& center, double rad, const Ray& r) {
+  vec3 oc = r.getOrigin() - center;
+  double a = dot(r.getDirection(),r.getDirection());
+  double b = 2 * dot(r.getDirection(),oc);
+  double c = dot(oc,oc) - rad*rad;
+  double discriminant = b*b - 4*a*c;
+  return (discriminant > 0);
+}
+
+// This function checks what the ray hits
 vec3 color(const Ray& r) {
+
+  // Create a sphere at (0,0,-1) with r=0.5
+  if (hit_sphere(vec3(0,0,-1), 0.5, r)) {
+    return vec3(1,0,0); // Sphere is colored red
+  }
+
+  // If nothing is hit, give the gradient background
   vec3 unit_direction = unit_vector(r.getDirection());
   double t = 0.5*(unit_direction.y() + 1);
   return (1-t)*vec3(1,1,1) + t*vec3(0.5,0.7,1.0);
